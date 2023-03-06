2bca8a094857e345274feb00866b1a1836d504e1
dinaledvina <d.ledvina@gmail.com>
2023-03-06 15:44:02 -0500

    Add files via upload

---
+++ utest_rect-no_input_06.py
@@ -0,0 +1,51 @@
+# import unittest
+# from rect_no_input_06 import Rectangle
+
+# class TestRectangle(unittest.TestCase):
+
+#     def setUp(self):
+#         self.rectangle = Rectangle()
+#         self.rectangle.length = 5
+#         self.rectangle.width = 10
+
+#     def test_GetLength(self):
+#         self.assertEqual(self.rectangle.GetLength(), 5)
+
+#     def test_GetWidth(self):
+#         self.assertEqual(self.rectangle.GetWidth(), 10)
+
+#     def test_GetArea(self):
+#         self.assertEqual(self.rectangle.GetArea(), 50)
+
+#     def test_GetPerimeter(self):
+#         self.assertEqual(self.rectangle.GetPerimeter(), 30)
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+
+
+
+
+
+import unittest
+from rect_no_input_06 import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+        self.rect = Rectangle(5, 10)
+    
+    def test_GetLength(self):
+        self.assertEqual(self.rect.GetLength(), 5)
+    
+    def test_GetWidth(self):
+        self.assertEqual(self.rect.GetWidth(), 10)
+    
+    def test_GetArea(self):
+        self.assertEqual(self.rect.GetArea(), 50)
+    
+    def test_GetPerimeter(self):
+        self.assertEqual(self.rect.GetPerimeter(), 30)
+
+if __name__ == '__main__':
+    unittest.main()
