a4a89552db4f5036c6a84d59f304c5acf0397173
dinaledvina <d.ledvina@gmail.com>
2023-03-07 12:02:47 -0500

    Update utest_rectangle_square_behavior.py

--- utest_rectangle_square_behavior.py
+++ utest_rectangle_square_behavior.py
@@ -1,46 +1,25 @@
-# import unittest
-# from rectangle import Rectangle
-# from square import Square
-
-# class TestSquare(unittest.TestCase):
-#     def test_same_behavior_as_rectangle(self):
-#         rect = Rectangle(5, 10)
-#         square = Square(5)
-
-#         self.assertEqual(rect.GetLength(), square.GetLength())
-#         self.assertEqual(rect.GetWidth(), square.GetWidth())
-#         self.assertEqual(rect.GetArea(), square.GetArea())
-#         self.assertEqual(rect.GetPerimeter(), square.GetPerimeter())
-
-
-# if __name__ == '__main__':
-#     unittest.main()
-
-
-
-
-
-
-
-
 import unittest
 from rectangle import Rectangle
 from square import Square
 
-
-class TestRectangleAndSquare(unittest.TestCase):
-    def test_rectangle_and_square(self):
+class TestSquare(unittest.TestCase):
+    def test_same_behavior_as_rectangle(self):
         rect = Rectangle(5, 10)
         square = Square(5)
-        self.assertEqual(rect.GetLength(), 5)
-        self.assertEqual(rect.GetWidth(), 10)
-        self.assertEqual(rect.GetArea(), 50)
-        self.assertEqual(rect.GetPerimeter(), 30)
-        self.assertEqual(square.GetLength(), 5)
-        self.assertEqual(square.GetWidth(), 5)
-        self.assertEqual(square.GetArea(), 25)
-        self.assertEqual(square.GetPerimeter(), 20)
+
+        self.assertEqual(rect.GetLength(), square.GetLength())
+        self.assertEqual(rect.GetWidth(), square.GetWidth())
+        self.assertEqual(rect.GetArea(), square.GetArea())
+        self.assertEqual(rect.GetPerimeter(), square.GetPerimeter())
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+
+
+
+
