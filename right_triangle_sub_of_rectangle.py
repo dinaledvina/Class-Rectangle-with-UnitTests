c66433ff03c55e905e59ed75a4310fb2561cfa22
dinaledvina <d.ledvina@gmail.com>
2023-03-07 15:29:23 -0500

    Add files via upload

---
+++ right_triangle_sub_of_rectangle.py
@@ -0,0 +1,22 @@
+from rect_no_input_06 import Rectangle
+
+
+
+
+
+class RightTriangle(Rectangle):
+    def __init__(self, base, height):
+        super().__init__(base, height)
+    
+    def GetArea(self):
+        return 0.5 * self.length * self.width
+    
+    def GetPerimeter(self):
+        return self.length + self.width + (self.length ** 2 + self.width ** 2) ** 0.5
+
+triangle = RightTriangle(3, 4)
+
+print("Base:", triangle.GetLength())
+print("Height:", triangle.GetWidth())
+print("Area:", triangle.GetArea())
+print("Perimeter:", triangle.GetPerimeter())
