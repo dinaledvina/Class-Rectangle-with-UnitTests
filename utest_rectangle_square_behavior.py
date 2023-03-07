c2d8fefcf102f870743c62fed363389c3055cb0b
dinaledvina <d.ledvina@gmail.com>
2023-03-07 10:52:05 -0500

    Update utest_rectangle_square_behavior.py

--- utest_rectangle_square_behavior.py
+++ utest_rectangle_square_behavior.py
@@ -24,8 +24,8 @@
 
 
 import unittest
-from rect_no_input_06 import Rectangle
-from sq_subclass_of_rect import Square
+from rectangle import Rectangle
+from square import Square
 
 
 class TestRectangleAndSquare(unittest.TestCase):
