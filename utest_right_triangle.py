e32231202db516f304fa91b31ffdeeaec030eb46
dinaledvina <d.ledvina@gmail.com>
2023-03-07 16:22:56 -0500

    Update utest_right_triangle.py

--- utest_right_triangle.py
+++ utest_right_triangle.py
@@ -3,17 +3,22 @@ from rectangle import Rectangle
 from right_triangle import RightTriangle
 
 
-
 class TestRightTriangle(unittest.TestCase):
-    def test_same_behavior_as_rectangle(self):
-        rect = Rectangle(5, 10)
-        rt = RightTriangle(3, 4)
-    
-        self.assertEqual(rect.GetLength(), rt.GetLength())
-        self.assertEqual(rect.GetWidth(), rt.GetWidth())
-        self.assertEqual(rect.GetArea(), rt.GetArea())
-        self.assertEqual(rect.GetPerimeter(), rt.GetPerimeter())
+    def setUp(self):
+        self.triangle = RightTriangle(3, 4)
 
+    def test_get_area(self):
+        self.assertEqual(self.triangle.GetArea(), 6.0)
+
+    def test_get_perimeter(self):
+        self.assertEqual(self.triangle.GetPerimeter(), 12.0)
+
+    def test_get_length(self):
+        self.assertEqual(self.triangle.GetLength(), 3)
+
+    def test_get_width(self):
+        self.assertEqual(self.triangle.GetWidth(), 4)
+        
 
 if __name__ == '__main__':
     unittest.main()
