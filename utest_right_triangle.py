6a9a090f215119bcd93ba6ca9ff71dd9f4a4e079
dinaledvina <d.ledvina@gmail.com>
2023-03-07 17:11:12 -0500

    Update utest_right_triangle.py

--- utest_right_triangle.py
+++ utest_right_triangle.py
@@ -10,14 +10,7 @@ class TestRightTriangle(unittest.TestCase):
     def test_get_area(self):
         self.assertEqual(self.triangle.GetArea(), 6.0)
 
-#     def test_get_perimeter(self):
-#         self.assertEqual(self.triangle.GetPerimeter(), 12.0)
 
-#     def test_get_length(self):
-#         self.assertEqual(self.triangle.GetLength(), 3)
-
-#     def test_get_width(self):
-#         self.assertEqual(self.triangle.GetWidth(), 4)
         
 
 if __name__ == '__main__':
