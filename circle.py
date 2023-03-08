3727e4654dea55e2c35f1d3d0a553686f89383b3
dinaledvina <d.ledvina@gmail.com>
2023-03-07 21:38:02 -0500

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,25 +1,5 @@
-from rect_no_input_06 import Rectangle
+from rectangle import Rectangle
 import math
-
-
-# class Circle(Rectangle):
-#     def __init__(self, radius):
-#         super().__init__(2 * radius, 2 * radius)
-#         self.radius = radius
-
-#     def GetLength(self):
-#         return 2 * self.radius
-
-#     def GetWidth(self):
-#         return 2 * self.radius
-
-#     def GetArea(self):
-#         return math.pi * self.radius ** 2
-
-#     def GetPerimeter(self):
-#         return 2 * math.pi * self.radius
-
-
 
 
 class Circle(Rectangle):
