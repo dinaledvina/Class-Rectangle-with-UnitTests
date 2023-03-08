c2736c1fbc7018b7f8a7d08b333fbae000a8fd44
dinaledvina <d.ledvina@gmail.com>
2023-03-08 16:07:14 -0500

    Update e_polygon.py

--- e_polygon.py
+++ e_polygon.py
@@ -8,16 +8,16 @@ class EquilateralPolygon:
     def __init__(self, num_sides):
         self.num_sides = num_sides
 
-    def perimeter(self):
+    def GetPerimeter(self):
         return self.num_sides * self.side_length
 
-    def area(self):
+    def GetArea(self):
         apothem = self.side_length / (2 * (math.tan(math.pi / self.num_sides)))
-        return (self.perimeter() * apothem) / 2
+        return (self.GetPerimeter() * apothem) / 2
 
 
 EquilateralPolygon.side_length = 5
 
 triangle = EquilateralPolygon(3)
-print("Triangle area:", triangle.area())
-print("Triangle perimeter:", triangle.perimeter())
+print("Triangle area:", triangle.GetArea())
+print("Triangle perimeter:", triangle.GetPerimeter())
