3f346a20ffa8dde87bb9e2796077319e916f3528
dinaledvina <d.ledvina@gmail.com>
2023-03-08 16:12:17 -0500

    Update square.py

--- square.py
+++ square.py
@@ -1,13 +1,18 @@
-from rectangle import Rectangle
+
+from e_polygon import EquilateralPolygon
+import math
+
+class Square(EquilateralPolygon):
+    def __init__(self):
+        super().__init__(4)
+
+    def GetArea(self):
+        return self.side_length ** 2
+
+    def GetPerimeter(self):
+        return self.num_sides * EquilateralPolygon.side_length
 
 
-class Square(Rectangle):
-    def __init__(self, side_length):
-        super().__init__(side_length, side_length)
-
-square = Square(5)
-
-print("Length:", square.GetLength())     
-print("Width:", square.GetWidth())       
-print("Area:", square.GetArea())        
-print("Perimeter:", square.GetPerimeter())
+square = Square()
+print("Square area:", square.GetArea())
+print("Square perimeter:", square.GetPerimeter())
