a5db14395f3d99e180a946624550ab9fc02c98ea
dinaledvina <d.ledvina@gmail.com>
2023-03-08 15:48:28 -0500

    Update e_polygon.py

--- e_polygon.py
+++ e_polygon.py
@@ -1,25 +1,5 @@
 import math
 
-# class EquilateralPolygon:
-#     def __init__(self, num_sides, side_length):
-#         self.num_sides = num_sides
-#         self.side_length = side_length
-
-#     def GetPerimeter(self):
-#         return self.num_sides * self.side_length
-
-#     def GetArea(self):
-#         apothem = self.side_length / (2 * (math.tan(math.pi / self.num_sides)))
-#         return (self.GetPerimeter() * apothem) / 2
-
-
-# my_epoly = EquilateralPolygon(3, 5)
-
-# print(my_epoly.GetArea())      
-# print(my_epoly.GetPerimeter())
-
-
-
 
 
 class EquilateralPolygon:
