0e527cc052fe68165fe2a884fc7b65b68b1dd9d8
dinaledvina <d.ledvina@gmail.com>
2023-03-07 21:40:12 -0500

    Add files via upload

---
+++ circle_test_rect_behavior.py
@@ -0,0 +1,21 @@
+import unittest
+import math
+from rect_no_input_06 import Rectangle
+from circle import Circle
+
+
+class TestCircle(unittest.TestCase):
+    def setUp(self):
+        self.my_circle = Circle(5)
+
+    def test_GetPerimeter(self):
+    	self.assertEqual(self.my_circle.GetPerimeter(), 30)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
