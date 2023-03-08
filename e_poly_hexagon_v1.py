0295fc5f87f576324dafd7c3283c2317217409a3
dinaledvina <d.ledvina@gmail.com>
2023-03-08 16:29:08 -0500

    Update e_poly_hexagon_v1.py

--- e_poly_hexagon_v1.py
+++ e_poly_hexagon_v1.py
@@ -3,15 +3,21 @@ import math
 
 
 class Hexagon(EquilateralPolygon):
-    def GetArea(self):
-        return 3 * self.side_length ** 2 * math.sqrt(3) / 2
-
-    def GetPerimeter(self):
-        return 6 * self.side_length
+	def __init__(self):
+		super().__init__(6)
 
 
 
-hexagon = Hexagon(6)
+	def GetArea(self):
+		return 3 * self.side_length ** 2 * math.sqrt(3) / 2
+
+
+	def GetPerimeter(self):
+		return 6 * self.side_length
+
+
+
+hexagon = Hexagon()
 
 print("Hexagon area:", hexagon.GetArea())
 print("Hexagon perimeter:", hexagon.GetPerimeter())
