837d034ca44e5a48458f9f413c66eec851ed7046
dinaledvina <d.ledvina@gmail.com>
2023-03-08 12:29:56 -0500

    Update hexagon.py

--- hexagon.py
+++ hexagon.py
@@ -5,6 +5,10 @@ from rectangle import Rectangle
 class Hexagon(Rectangle):
     def __init__(self, side):
         super().__init__(side, side)
+        self.side = side
+
+    def GetArea(self):
+    	return (3 * (3 ** 0.5) / 2) * self.side ** 2
 
 
 hexagon = Hexagon(5)
