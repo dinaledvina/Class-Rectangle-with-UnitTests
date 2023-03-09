b00992218255e06ce4e1bbf0b2f57c4b23ab0732
dinaledvina <d.ledvina@gmail.com>
2023-03-09 00:18:03 -0500

    Update utest_hex.py

--- utest_hex.py
+++ utest_hex.py
@@ -8,12 +8,7 @@ from epolygon_hexagon import Hexagon
 class TestHexagon(unittest.TestCase):
 
   
-#REMOVING commented out lines in github for now
 
-   # def length_and_width(self):
-        #hexagon = Hexagon(5)
-        #self.assertEqual(hexagon.GetLength(), 5)
-        #self.assertEqual(hexagon.GetWidth(), 5)
 
     def test_area(self):
         hexagon = Hexagon(5)
@@ -29,25 +24,5 @@ if __name__ == '__main__':
 
 
 
-#WORKING UTEST FOR CLASS HEXAGON
 
 
-# class TestHexagon(unittest.TestCase):
-
-#     def test_length_and_width(self):
-#         hexagon = Hexagon(5)
-#         self.assertEqual(hexagon.GetLength(), 5)
-#         self.assertEqual(hexagon.GetWidth(), 5)
-
-#     def test_area(self):
-#         hexagon = Hexagon(5)
-#         self.assertAlmostEqual(hexagon.GetArea(), 64.95, places=2)
-
-#     def test_perimeter(self):
-#         hexagon = Hexagon(5)
-#         self.assertEqual(hexagon.GetPerimeter(), 30)
-
-        
-
-# if __name__ == '__main__':
-#     unittest.main()
