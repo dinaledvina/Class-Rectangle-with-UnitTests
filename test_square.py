01cc328dcc178947c876946c155f73c76afdd4db
dinaledvina <d.ledvina@gmail.com>
2023-03-09 16:07:13 -0500

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -5,11 +5,9 @@ class TestSquare(unittest.TestCase):
 
 
     def test_area(self):
-        square = Square(4)
     	self.assertEqual(square.GetArea(), 25)
 
     def test_perimeter(self):
-        square = Square(4)
         self.assertEqual(square.GetPerimeter(), 20)
 
 
