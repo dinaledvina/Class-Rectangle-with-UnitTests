08bda18a3661e9aaf50b7f9f3878d5137dd1590c
dinaledvina <d.ledvina@gmail.com>
2023-03-10 14:52:50 -0500

    Update test_hexagon.py

--- test_hexagon.py
+++ test_hexagon.py
@@ -1,20 +1,19 @@
 import unittest
-from polygon_hexagon import Hexagon
+from e_polygon import EquilateralPolygon
+from epolygon_hexagon import Hexagon
 
 
 
 class TestHexagon(unittest.TestCase):
 
-  
-
 
     def test_area(self):
-      hexagon = Hexagon(5)
-      self.assertAlmostEqual(GetArea(), 64.95, places=2)
+        
+        self.assertAlmostEqual(hexagon.GetArea(), 64.95, places=2)
 
     
 
-
+hexagon = Hexagon(6, 5)
 
 if __name__ == '__main__':
     unittest.main()
