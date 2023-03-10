4571214cbaddb83ede76074b51ced94036d7e989
dinaledvina <d.ledvina@gmail.com>
2023-03-10 14:53:52 -0500

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -1,20 +1,17 @@
+
 import unittest
-from polygon_square import Square
+from epolygon_square import Square
 
 class TestSquare(unittest.TestCase):
 
+	def test_area(self):
+		self.assertEqual(square.GetArea(), 25)
 
-    def test_area(self):
-        square = Square(4)
-    	self.assertEqual(square.GetArea(), 25)
-
-    def test_perimeter(self):
-        square = Square(4)
-        self.assertEqual(square.GetPerimeter(), 20)
+	def test_perimeter(self):
+		self.assertEqual(square.GetPerimeter(),20)
 
 
-
-
+square = Square(4, 5)
 
 if __name__ == '__main__':
     unittest.main()
