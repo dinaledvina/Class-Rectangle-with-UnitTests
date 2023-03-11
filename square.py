69b500eb303bdbdd246b3b1df606491191f64a0a
dinaledvina <d.ledvina@gmail.com>
2023-03-10 23:13:25 -0500

    Update square.py

--- square.py
+++ square.py
@@ -5,8 +5,7 @@ class Square(EquilateralPolygon):
     def __init__(self, side_length):
         super().__init__(4, side_length)
 
-    def GetArea(self):
-        return self.side_length ** 2
+
 
 
 
