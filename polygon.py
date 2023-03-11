e141dd1db3d287fb9aab2e0680501e12e9a4922b
dinaledvina <d.ledvina@gmail.com>
2023-03-10 20:36:19 -0500

    Update polygon.py

--- polygon.py
+++ polygon.py
@@ -1,20 +1,14 @@
-
 class EquilateralPolygon:
-   
-
     def __init__(self, num_sides, side_length):
         self.num_sides = num_sides
         self.side_length = side_length
 
     def GetPerimeter(self):
-        polygon = EquilateralPolygon(3,5)
         return self.num_sides * self.side_length
 
     def GetArea(self):
-        polygon = EquilateralPolygon(3,5)
         apothem = self.side_length / (2 * (math.tan(math.pi / self.num_sides)))
         return (self.GetPerimeter() * apothem) / 2
-
 
 
 
