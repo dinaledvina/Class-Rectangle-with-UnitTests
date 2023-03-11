6b898fe9942b912dba4332997fd92e3f3b42a5de
dinaledvina <d.ledvina@gmail.com>
2023-03-11 16:19:22 -0500

    Update hexagon.py

--- hexagon.py
+++ hexagon.py
@@ -3,7 +3,7 @@ from polygon import EquilateralPolygon
 
 
 class Hexagon(EquilateralPolygon):
-	def __init__(self, num_sides, side_length):
+	def __init__(self, side_length):
 		super().__init__(6, side_length)
 
 
