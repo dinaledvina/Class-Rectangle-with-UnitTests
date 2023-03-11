24e95ab3d9f989a48ffd5b3c6707e24cc8b9d7b5
dinaledvina <d.ledvina@gmail.com>
2023-03-11 16:14:42 -0500

    Update test_hexagon.py

--- test_hexagon.py
+++ test_hexagon.py
@@ -5,11 +5,11 @@ from hexagon import Hexagon
 class TestHexagon(unittest.TestCase):
 
     def test_area(self):
-        hexagon = Hexagon(6, 5)
+        hexagon = Hexagon(5)
         self.assertAlmostEqual(hexagon.GetArea(), 64.95, places=2)
 
     def test_perimeter(self):
-        hexagon = Hexagon(6, 5)
+        hexagon = Hexagon(5)
         self.assertEqual(hexagon.GetPerimeter(),30)
 
 if __name__ == '__main__':
