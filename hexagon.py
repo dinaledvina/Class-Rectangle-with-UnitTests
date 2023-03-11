7f86422261ca50d72e135ff853b42771f8b40d65
dinaledvina <d.ledvina@gmail.com>
2023-03-10 20:39:41 -0500

    Update hexagon.py

--- hexagon.py
+++ hexagon.py
@@ -4,13 +4,16 @@ import math
 
 class Hexagon(EquilateralPolygon):
 	def __init__(self, num_sides, side_length):
-		super().__init__(num_sides, side_length)
+		super().__init__(6, side_length)
 
 
 
 	def GetArea(self):
-		hexagon = Hexagon(6, 5)
 		return 3 * self.side_length ** 2 * math.sqrt(3) / 2
+
+
+	def GetPerimeter(self):
+		return super().GetPerimeter()
 
 
 
