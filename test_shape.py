8b9333933ccbfc73553adfe6496f39c74649e092
dinaledvina <d.ledvina@gmail.com>
2023-03-13 18:40:53 -0400

    Update test_shape.py

--- test_shape.py
+++ test_shape.py
@@ -1,14 +1,22 @@
+
 import unittest
 from shape import Shape
 
 class TestShape(unittest.TestCase):
     def test_init(self):
-        
-        shape = Shape()
-
        
-        self.assertEqual(shape.x, 10)
-        self.assertEqual(shape.y, 10)
+        shape = Shape()
+        x_value = 0
+        y_value = 0
+
+    def test_set_position(self):
+   
+        shape.set_position(x_value, y_value)
+        self.assertEqual(shape.get_position(), (x_value, y_value))
+
+    def test_get_position(self):
+      
+        self.assertEqual(shape.get_position(), (x_value, y_value))
 
 if __name__ == '__main__':
     unittest.main()
