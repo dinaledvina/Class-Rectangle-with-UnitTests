43de9f6251c2c60d6b2c786da1ec372327614679
dinaledvina <d.ledvina@gmail.com>
2023-03-13 16:28:40 -0400

    Update test_shape.py

--- test_shape.py
+++ test_shape.py
@@ -6,8 +6,7 @@ class TestShape(unittest.TestCase):
     def test_shape_position(self):
         shape = Shape()
         
-        shape.x = 10
-        shape.y = 10
+        
         
         shape.set_position(shape.x, shape.y)
         
