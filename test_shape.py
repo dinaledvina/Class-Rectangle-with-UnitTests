f8fd6994458354335fed339d9289ce6b0b8c453f
dinaledvina <d.ledvina@gmail.com>
2023-03-13 15:48:29 -0400

    Update test_shape.py

--- test_shape.py
+++ test_shape.py
@@ -5,11 +5,11 @@ class TestShape(unittest.TestCase):
 
     def test_shape_position(self):
         shape = Shape()
+        
+        shape.set_position()
+        
+        self.assertEqual(shape.get_position()
 
-
-        set_position(shape.x, shape.y)
-       
-        self.assertEqual(get_position(), (shape.x, shape.y))
         
         
         
