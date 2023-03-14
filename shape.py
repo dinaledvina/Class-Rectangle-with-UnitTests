6ff6bab4b06bddf9364d90a8a39e172883988792
dinaledvina <d.ledvina@gmail.com>
2023-03-13 22:17:31 -0400

    Update shape.py

--- shape.py
+++ shape.py
@@ -1,6 +1,20 @@
-class Shape:
-    def __init__(self):
-        self.__x = 0
-        self.__y = 0
 
+class Shape:
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
+    
+    def set_position(self, x_value, y_value):
+        self.__x = x_value
+        self.__y = y_value
+    
+    def get_position(self):
+        return (self.__x, self.__y)
+
+
+
+my_shape = Shape()
+my_shape.set_position(3, 4)
+position = my_shape.get_position()
+print(position)
  
