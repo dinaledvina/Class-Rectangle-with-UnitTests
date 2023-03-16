41958c231f84921deac962f5f720ce64609f0c95
dinaledvina <d.ledvina@gmail.com>
2023-03-16 15:04:26 -0400

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -3,10 +3,12 @@ from shape import Shape
 
 
 class Rectangle(Shape):
-    def __init__(self, length, width, x = 0, y = 0):
-        super().__init__(x, y)
+    def __init__(self, length, width):
+        super().__init__()
         self.length = length
         self.width = width
+        
+        self.corners = ((0,0), (self.length, 0), (self.length, self.width), (0, self.width))
     
     def GetLength(self):
         return self.length
@@ -19,5 +21,10 @@ class Rectangle(Shape):
     
     def GetPerimeter(self):
         return 2 * (self.length + self.width)
+    
 
+    def set_corners(self, corner1, corner2):
+        self.corners = (corner1, corner2)
 
+    def get_corners(self):
+        return self.corners
