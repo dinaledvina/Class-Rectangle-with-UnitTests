db491580e5d32e3269d2f37454937bf0ff23084c
dinaledvina <d.ledvina@gmail.com>
2023-03-16 00:15:21 -0400

    Add files via upload

---
+++ test_rectangle_shape.py
@@ -0,0 +1,29 @@
+
+import unittest
+from rect_no_input_06 import Rectangle
+
+class TestRectangle(unittest.TestCase):
+    def setUp(self):
+        self.rect = Rectangle(5, 10)
+    
+    def test_GetLength(self):
+        self.assertEqual(self.rect.GetLength(), 5)
+    
+    def test_GetWidth(self):
+        self.assertEqual(self.rect.GetWidth(), 10)
+    
+    def test_GetArea(self):
+        self.assertEqual(self.rect.GetArea(), 50)
+    
+    def test_GetPerimeter(self):
+        self.assertEqual(self.rect.GetPerimeter(), 30)
+
+
+    def test_set_get_corners(self):
+        self.rect.set_corners((0, 0), (5, 10))
+        self.assertEqual(self.rect.get_corners(), ((0, 0), (5, 10)))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
