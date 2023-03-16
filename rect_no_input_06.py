7cf1f861f2eb3bf3a9e8cbd10ae04776853bbad7
dinaledvina <d.ledvina@gmail.com>
2023-03-16 00:22:24 -0400

    Update rect_no_input_06.py

--- rect_no_input_06.py
+++ rect_no_input_06.py
@@ -21,23 +21,3 @@ class Rectangle(Shape):
         return 2 * (self.length + self.width)
 
 
-#### Added methods from super class Shape
-    # def set_corners(self, top_left, bottom_right):
-    #     self.top_left = top_left
-    #     self.bottom_right = bottom_right
-    
-    # def get_corners(self):
-    #     return (self.top_left, self.bottom_right)
-
-
-
-
-
-
-rect = Rectangle(5, 10)
-
-print("Length:", rect.GetLength())     
-print("Width:", rect.GetWidth())       
-print("Area:", rect.GetArea())        
-print("Perimeter:", rect.GetPerimeter())   
-
