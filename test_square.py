67cd45d62e545f12f884634770d66e5c80cc93bb
dinaledvina <d.ledvina@gmail.com>
2023-03-18 15:18:50 -0400

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -10,6 +10,21 @@ class TestSquare(unittest.TestCase):
 	def test_perimeter(self):
 		square = Square(5)
 		self.assertEqual(square.GetPerimeter(),20)
+		
+		
+	def test_set_length(self):
+		square = Square(7)
+		square.set_length(7)
+		self.assertEqual(square.side_length, 7)
+
+		self.assertEqual(square.corners, ((0,0), (7, 0), (7, 7), (0, 7)))
+
+
+	def test_set_corners(self):
+		square = Square(7)
+		square.set_corners((0,0), (7,7))
+		self.assertEqual(square.get_corners(), ((0,0), (7,0), (7,7), (0,7)))
+		self.assertEqual(square.side_length, 7)
 
 if __name__ == '__main__':
     unittest.main()
