808d8e80dd2121ed1a86514ba4dec0cc805eeb06
dinaledvina <d.ledvina@gmail.com>
2023-03-19 20:23:42 -0400

    Update square.py

--- square.py
+++ square.py
@@ -1,28 +1,23 @@
-from polygon import EquilateralPolygon
+from rectangle import Rectangle
 
-class Square(EquilateralPolygon):
+
+class Square(Rectangle):
     def __init__(self, side_length):
-        super().__init__(4, side_length)
+        self.side_length = side_length
         self.corners = ((0,0), (self.side_length, 0), (self.side_length, self.side_length), (0, self.side_length))
-
-
+        super().__init__(side_length, side_length)
 
     def set_length(self, length):
         self.side_length = length
         self.corners = ((0,0), (self.side_length, 0), (self.side_length, self.side_length), (0, self.side_length))
 
-
     def set_corners(self, corner1, corner2):
         (x1, y1), (x2, y2) = corner1, corner2
-        self.length = x2 - x1
-        self.length = y2 - y1
+        self.side_length = x2 - x1
         self.corners = (corner1, (x2, y1), corner2, (x1, y2))
 
-
-
     def get_corners(self):
-    	
-    	return self.corners
+        return self.corners
 
 
 
