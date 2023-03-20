d347af785349968e2171eac61bf4e32b86344a5b
dinaledvina <d.ledvina@gmail.com>
2023-03-19 20:26:59 -0400

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -1,30 +1,40 @@
+
 import unittest
+from rectangle import Rectangle
 from square import Square
 
 class TestSquare(unittest.TestCase):
+    def setUp(self):
+        self.square = Square(5)
 
-	def test_area(self):
-		square = Square(5)
-		self.assertAlmostEqual(square.GetArea(), 25)
+    def test_get_length(self):
+        self.assertEqual(self.square.GetLength(), 5)
 
-	def test_perimeter(self):
-		square = Square(5)
-		self.assertEqual(square.GetPerimeter(),20)
-		
-		
-	def test_set_length(self):
-		square = Square(7)
-		square.set_length(7)
-		self.assertEqual(square.side_length, 7)
+    def test_get_width(self):
+        self.assertEqual(self.square.GetWidth(), 5)
 
-		self.assertEqual(square.corners, ((0,0), (7, 0), (7, 7), (0, 7)))
+    def test_get_area(self):
+        self.assertEqual(self.square.GetArea(), 25)
 
+    def test_get_perimeter(self):
+        self.assertEqual(self.square.GetPerimeter(), 20)
 
-	def test_set_corners(self):
-		square = Square(7)
-		square.set_corners((0,0), (7,7))
-		self.assertEqual(square.get_corners(), ((0,0), (7,0), (7,7), (0,7)))
-		self.assertEqual(square.side_length, 7)
+    def test_set_length(self):
+    	self.square = Square(10)
+    	self.square.set_length(10)
+    	self.assertEqual(self.square.GetLength(), 10)
+    	self.assertEqual(self.square.GetWidth(), 10)
+    	self.assertEqual(self.square.GetArea(), 100)
+    	self.assertEqual(self.square.GetPerimeter(), 40)
+
+    def test_set_corners(self):
+    	self.square = Square(10)
+    	self.square.set_corners((0,0), (10,10))
+    	self.assertEqual(self.square.GetLength(), 10)
+    	self.assertEqual(self.square.GetWidth(), 10)
+    	self.assertEqual(self.square.GetArea(), 100)
+    	self.assertEqual(self.square.GetPerimeter(), 40)
+    	self.assertEqual(self.square.get_corners(), ((0,0), (10, 0), (10, 10), (0, 10)))
 
 if __name__ == '__main__':
     unittest.main()
