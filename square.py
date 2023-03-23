402f664dde7edfe2fbaf2c5e217d96fb4afb3f7e
dinaledvina <d.ledvina@gmail.com>
2023-03-23 00:23:39 -0400

    Update square.py

--- square.py
+++ square.py
@@ -4,7 +4,7 @@ from rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, side_length):
         self.side_length = side_length
-        self.corners = ((0,0), (self.side_length, 0), (self.side_length, self.side_length), (0, self.side_length))
+        
         super().__init__(side_length, side_length)
 
 
