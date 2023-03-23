cadcfac50f01041bacec5b0864ba294ab05a0d92
dinaledvina <d.ledvina@gmail.com>
2023-03-23 19:06:56 -0400

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -5,19 +5,5 @@ class TestTriangle(unittest.TestCase):
         t = Triangle(3, 4, 5)
         self.assertEqual(t.area(), 6.0)
 
-    # def test_is_equilateral(self):
-    #     t = Triangle(5, 5, 5)
-    #     self.assertTrue(t.is_equilateral())
-
-    #     t = Triangle(3, 4, 5)
-    #     self.assertFalse(t.is_equilateral())
-
-    # def test_is_isosceles(self):
-    #     t = Triangle(5, 5, 3)
-    #     self.assertTrue(t.is_isosceles())
-
-    #     t = Triangle(3, 4, 5)
-    #     self.assertFalse(t.is_isosceles())
-
 if __name__ == '__main__':
     unittest.main()
