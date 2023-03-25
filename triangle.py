7f6fa28a6c95746ccc2903deb7df14b739f8d2b1
dinaledvina <d.ledvina@gmail.com>
2023-03-24 22:49:08 -0400

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -7,3 +7,19 @@ class Triangle:
     def area(self):
         s = (self.a + self.b + self.c) / 2
         return (s * (s - self.a) * (s - self.b) * (s - self.c)) ** 0.5
+    
+    def perimeter(self):
+    	return (self.a + self.b + self.c)
+
+
+    def set_corners(self, corner1, corner2, corner3):
+        self.corners = (corner1, corner2, corner3)
+        
+    def get_corners(self):
+        return self.corners
+
+    def set_length(self, a_l, b_l, c_l):
+    	self.a = a_l
+    	self.b = b_l
+    	self.c = c_l
+    	self.corners = ((0,0), (self.a, 0), (0, self.b))
