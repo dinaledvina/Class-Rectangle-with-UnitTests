17c778352172b0db28a6d91d4201dfeea8e9a29d
dinaledvina <d.ledvina@gmail.com>
2023-03-24 22:50:16 -0400

    Update triangle_test.py

--- triangle_test.py
+++ triangle_test.py
@@ -4,6 +4,27 @@ class TriangleTest(unittest.TestCase):
     def test_area(self):
         t = Triangle(3, 4, 5)
         self.assertEqual(t.area(), 6.0)
+        
+    def test_perimeter(self):
+        t = Triangle(3, 4, 5)
+        self.assertEqual(t.perimeter(), 12)
+
+    def test_set_corners(self):
+        t = Triangle(0, 0, 0)
+        t.set_corners((0,0), (4,0), (0, 3))
+      
+        self.assertEqual(t.get_corners(), ((0,0), (4,0), (0, 3)))
+
+
+
+    def test_set_length(self):
+        t = Triangle(5, 5, 5)
+        t.set_length(6, 8, 10)
+        self.assertEqual(t.a, 6)
+        self.assertEqual(t.b, 8)
+        self.assertEqual(t.c, 10)
+
+        expected_corners = ((0,0), (6,0), (0,8))
 
 if __name__ == '__main__':
     unittest.main()
