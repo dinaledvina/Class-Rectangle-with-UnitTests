b3f8ed2efc5f3b3f8d176eb0bed408a6ccf07397
dinaledvina <d.ledvina@gmail.com>
2023-03-29 15:39:52 -0400

    Update right_triangle.py

--- right_triangle.py
+++ right_triangle.py
@@ -11,6 +11,9 @@ class RightTriangle:
     def area(self):
     	s = (self.a + self.b + self.c) / 2
     	return (s * (s - self.a) * (s - self.b) * (s - self.c)) ** 0.5
+    
+     def get_corners(self):
+        return self.corners
 
     def set_length(self, a, b, c):
     	self.corners = ((0,0), (self.a, 0), (0, self.b))
