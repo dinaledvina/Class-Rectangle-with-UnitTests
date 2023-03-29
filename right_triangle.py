9f17bf7a5bfc366cd94c71551617cd3f4cbbdc80
dinaledvina <d.ledvina@gmail.com>
2023-03-29 00:01:03 -0400

    Update right_triangle.py

--- right_triangle.py
+++ right_triangle.py
@@ -2,9 +2,7 @@
 
 class RightTriangle:
     def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+        self.a, self.b, self.c = a, b, c
         self.corners = ((0,0), (self.a, 0), (0, self.b))
 
     def perimeter(self):
@@ -14,15 +12,11 @@ class RightTriangle:
     	s = (self.a + self.b + self.c) / 2
     	return (s * (s - self.a) * (s - self.b) * (s - self.c)) ** 0.5
 
-
     def set_corners(self, corner1, corner2, corner3):
         self.corners = (corner1, corner2, corner3)
 
     def get_corners(self):
         return self.corners
 
-    def set_length(self, a_l, b_l, c_l):
-    	self.a = a_l
-    	self.b = b_l
-    	self.c = c_l
+    def set_length(self, a, b, c):
     	self.corners = ((0,0), (self.a, 0), (0, self.b))
