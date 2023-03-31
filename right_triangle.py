90a4a71f431e63e8d0b08bce3816d4e100a62b12
dinaledvina <d.ledvina@gmail.com>
2023-03-31 15:16:37 -0400

    Update right_triangle.py

--- right_triangle.py
+++ right_triangle.py
@@ -3,9 +3,10 @@ from triangle import Triangle
 
 class RightTriangle(Triangle):
     def __init__(self, a, b, c):
-        self.a, self.b, self.c = a, b, c
+        super().__init__(a, b, c)
         if self.a ** 2 + self.b ** 2 != self.c ** 2:
             raise ValueError("Not a right triangle")
         self.corners = ((0, 0), (self.a, 0), (0, self.b))
+
 
 
