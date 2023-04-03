07e6cf0418835dc02ece0d528456fffb8f232006
dinaledvina <d.ledvina@gmail.com>
2023-04-03 12:30:21 -0400

    Update right_triangle_test.py

--- right_triangle_test.py
+++ right_triangle_test.py
@@ -11,14 +11,6 @@ class TestRightTriangle(unittest.TestCase):
 	    t = RightTriangle(3, 4, 5)
 	    self.assertEqual(t.area(), 6.0)
 
-	def test_set_length(self):
-	    t = RightTriangle(6, 8, 10)
-	    self.assertEqual(t.a, 6)
-	    self.assertEqual(t.b, 8)
-	    self.assertEqual(t.c, 10)
-
-	    expected_corners = ((0,0), (6,0), (0,8))
-	    self.assertEqual(t.get_corners(), expected_corners)
 		
 	def test_set_length(self):
             t = RightTriangle(20, 20, 20)
