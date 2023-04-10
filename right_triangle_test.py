022ef28eeb99cd88c9887ca577356917b8ddd936
dinaledvina <d.ledvina@gmail.com>
2023-04-10 13:30:49 -0400

    Update right_triangle_test.py

--- right_triangle_test.py
+++ right_triangle_test.py
@@ -4,16 +4,16 @@ from right_triangle import RightTriangle
 class TestRightTriangle(unittest.TestCase):
 
 	def test_perimeter(self):
-	    t = RightTriangle(3, 4, 5)
+	    t = RightTriangle(3, 4)
 	    self.assertEqual(t.perimeter(), 12)
 
 	def test_area(self):
-	    t = RightTriangle(3, 4, 5)
+	    t = RightTriangle(3, 4)
 	    self.assertEqual(t.area(), 6.0)
 		
 	# test right triangle: setting sides length and asserting expected corners 
 	def test_set_length_right(self):
-	    t = RightTriangle(6, 8, 10)
+	    t = RightTriangle(6, 8)
 		
 	    self.assertEqual(t.a, 6)
 	    self.assertEqual(t.b, 8)
