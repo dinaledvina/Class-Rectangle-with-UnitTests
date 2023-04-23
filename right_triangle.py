d2678e5f113364b25309b9a6c41f5538bae3ef65
dinaledvina <d.ledvina@gmail.com>
2023-04-23 00:32:54 -0400

    Update right_triangle.py

--- right_triangle.py
+++ right_triangle.py
@@ -5,7 +5,7 @@ from triangle import Triangle
 class RightTriangle(Triangle):
     def __init__(self, a, b):
         c = (a ** 2 + b ** 2) ** 0.5
-        if a ** 2 + b ** 2 != c ** 2:
+        if not math.isclose(a ** 2 + b ** 2, c ** 2, rel_tol=1e-9):   
             raise ValueError("Not a right triangle")
         super().__init__(a, b, c)
         
